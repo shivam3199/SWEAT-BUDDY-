d8b50f3b0b364751b5f51c1eadb5b799c8316376
Shivam Raskonda <shivamraskonda589@gmail.com>
2023-06-13 23:46:32 +0530

    necessary changes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const path = require('path')
 
 // express app
 const app = express()
